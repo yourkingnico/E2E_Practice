1e27898048245d8b113e5450f68a5d3076eb3fb4
Nick Torres <nicos11@comcast.net>
2019-05-11 10:36:49 -0400

    add About Label check, rename page object

--- smokeTests/pages/HomePage.js
+++ smokeTests/pages/HomePage.js
@@ -1,9 +1,25 @@
 var HomePage = function(){
     var menu =  element(by.css('.navbar-nav'));
+    var homeButton = element(by.css('.navbar-brand'));
+    var introText = element(by.css('.intro-text'));
+    var aboutLabel = element(by.css('h2.home-titles:not(:first-child)'));
+
 
     this.isMenuVisible = function () {
         console.log("Looking for Menu");
         return menu.isDisplayed();
+    }
+
+    this.isHomeButtonVisible = function () {
+        return homeButton.isDisplayed();
+    }
+
+    this.isIntroTextVisible = function () {
+        return introText.isDisplayed();
+    }
+
+    this.isAboutLabelVisible = function () {
+        return aboutLabel.isDisplayed();
     }
 }
 module.exports = new HomePage();
--- smokeTests/specs/menuCheck.spec.js
+++ smokeTests/specs/menuCheck.spec.js
@@ -1,18 +1,21 @@
-var menu = require('../pages/HomePage')
+var HomePage = require('../pages/HomePage')
 
 
 describe('Load home page and check menu buttons', function() { 
 
   it('should load home page', function() { 
-    // Entering web url in browser
     browser.waitForAngularEnabled(false);
     browser.get('http://nicktorres.net');
     console.log("Home page loaded for nicktorres.net");
   })
 
   it('should check menu is visible', function() { 
-    expect(menu.isMenuVisible()).toBe(true);
+    expect(HomePage.isMenuVisible()).toBe(true);
     console.log("Menu found");
+  })
+
+  it('should check About label is visible', function() { 
+    expect(HomePage.isAboutLabelVisible()).toBe(true);
   })
 
 });
