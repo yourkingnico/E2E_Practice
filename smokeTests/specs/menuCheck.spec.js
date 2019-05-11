2b15b33dac095c366e0a752e7188828bd1ecf912
Nick Torres <nicos11@comcast.net>
2019-05-11 09:26:52 -0400

    update menuCheck to use page object

--- smokeTests/specs/menuCheck.spec.js
+++ smokeTests/specs/menuCheck.spec.js
@@ -1,15 +1,17 @@
+var menu = require('../pages/HomePage')
+
+
 describe('Load home page and check menu buttons', function() { 
 
   it('should load home page', function() { 
-    // Entering application url in browser
+    // Entering web url in browser
     browser.waitForAngularEnabled(false);
     browser.get('http://nicktorres.net');
     console.log("Home page loaded for nicktorres.net");
   })
 
-  it('should check menu', function() { 
-    var menu =  element(by.css('.navbar-nav'));
-    expect(menu.isDisplayed()).toBeTruthy();
+  it('should check menu is visible', function() { 
+    expect(menu.isMenuVisible()).toBe(true);
     console.log("Menu found");
   })
 
