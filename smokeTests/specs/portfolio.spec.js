5024f8e473ee2db0a44c8823eda336ed0a806bba
Nick Torres <nicos11@comcast.net>
2019-05-17 22:15:58 -0400

    add config and spec for portfolio

---
+++ smokeTests/specs/portfolio.spec.js
@@ -0,0 +1,19 @@
+var PortfolioPage = require('../pages/PortfolioPage');
+var Menu = require('../pages/sections/Menu');
+
+describe('Load portfolio page and check for essential ', function() { 
+
+  it('should load home page', function() { 
+    browser.waitForAngularEnabled(false);
+    browser.get('http://nicktorres.net');
+    console.log("Home page loaded for nicktorres.net");
+  })
+
+  it('should naviagate the the portfolio page', function() { 
+    expect(Menu.openPortfolioPage()).toBeTruthy();
+  })
+
+  it('should verify portfolio label is visible', function() { 
+    expect(PortfolioPage.isPortfolioLabelVisible()).toBeTruthy();
+  })
+});
